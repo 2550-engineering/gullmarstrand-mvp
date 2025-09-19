328a641176229c393106e8720c4d9a8cbc8b7d74
philipwaagistrom <philip.waagistrom@2550.engineering>
2025-09-19 17:26:50 +0200

    Create a user listing (#6)
    * First draft of listings
    
    * Add support for viewing a couple of pictures
    
    * Add a create listings page
    
    * Move listing enpoints to separate file from main (this is how its done in the new backend)
    
    * Ignore pyc files
    
    * Working version of listings on top of new backend :-)
    
    ---------
    
    Co-authored-by: LinusLagergren <linus.lagergren@2550.engineering>

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,29 @@
 from fastapi import FastAPI
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
+
+from backend.models import Base
+from backend.listings import router as listings_router
 from backend.auth import router as auth_router
 
+DATABASE_URL = "sqlite:///marketplace.db"
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(bind=engine)
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Include routers
+app.include_router(listings_router)
 app.include_router(auth_router)
 
 @app.get("/", response_class=HTMLResponse)
